a80cbb47967ee01e68e591219b0fd151f09348e5
bigal <bigal_24@hotmail.com>
2018-04-12 20:53:42 -0700

    Start

---
+++ Source/Editor/Converters/ObjectTypeNameToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Editor.Converters
+{
+    public class ObjectTypeNameToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? value.GetType().Name : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Source/Editor/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Repository;
+using Microsoft.Win32;
+
+namespace Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            xChildren.ItemsSource = new[] { new ConsoleAppender { Name = "console-test" } };
+        }
+
+        private void OpenOnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Config Files (*.xml, *.config) | *.xml; *.config" };
+
+            bool? showDialog = ofd.ShowDialog(this);
+
+            if (showDialog.Value)
+            {
+                XmlConfigurator.Configure(new FileInfo(ofd.FileName));
+
+                ILoggerRepository loggerRepository = LogManager.GetRepository();
+                IAppender[] appenders = loggerRepository.GetAppenders();
+            }
+        }
+    }
+}
