422e6ed597ed1d7d4d3715de6ffb7c73d608da4f
alexleen <24834773+alexleen@users.noreply.github.com>
2019-08-14 06:30:23 -0500

    Start on #58

--- Source/Editor/ConfigProperties/Base/MultiValuePropertyBase.cs
+++ Source/Editor/ConfigProperties/Base/MultiValuePropertyBase.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2018 Alex Leendertsen
+﻿// Copyright © 2019 Alex Leendertsen
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,7 +7,7 @@ using Editor.Interfaces;
 
 namespace Editor.ConfigProperties.Base
 {
-    internal abstract class MultiValuePropertyBase<TValueType> : PropertyBase
+    public abstract class MultiValuePropertyBase<TValueType> : PropertyBase
     {
         protected MultiValuePropertyBase(ReadOnlyCollection<IProperty> container, GridLength rowHeight, string name, IEnumerable<TValueType> values, double width)
             : base(container, rowHeight)
--- Source/Editor/ConfigProperties/TypeAttribute.cs
+++ Source/Editor/ConfigProperties/TypeAttribute.cs
@@ -1,42 +1,46 @@
-﻿// Copyright © 2018 Alex Leendertsen
+﻿// Copyright © 2019 Alex Leendertsen
 
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Xml;
 using Editor.ConfigProperties.Base;
 using Editor.Descriptors;
 using Editor.Interfaces;
-using Editor.Utilities;
 
 namespace Editor.ConfigProperties
 {
-    public class TypeAttribute : StringValueProperty
+    public class TypeAttribute : MultiValuePropertyBase<AppenderDescriptor>
     {
         private const string TypeName = "type";
 
+        private static readonly AppenderDescriptor[] sValues =
+        {
+            AppenderDescriptor.Async
+        };
+
         public TypeAttribute(ReadOnlyCollection<IProperty> container)
-            : base(container, "Type:", null)
+            : base(container, GridLength.Auto, "Type:", sValues, 100)
         {
         }
 
         public TypeAttribute(ReadOnlyCollection<IProperty> container, AppenderDescriptor descriptor)
-            : base(container, "Type:", null)
+            : base(container, GridLength.Auto, "Type:", sValues, 100)
         {
-            //Will be overwritten when/if load is called
-            Value = descriptor.TypeNamespace;
-            IsReadOnly = true;
+            //Will be overwritten when/if load is called (?)
+            SelectedValue = descriptor;
         }
 
         public override void Load(XmlNode originalNode)
         {
-            SetValueIfNotNullOrEmpty(originalNode.Attributes[TypeName]?.Value);
+            //SetValueIfNotNullOrEmpty(originalNode.Attributes[TypeName]?.Value);
         }
 
         public override void Save(XmlDocument xmlDoc, XmlNode newNode)
         {
-            if (!string.IsNullOrEmpty(Value))
-            {
-                newNode.AppendAttribute(xmlDoc, TypeName, Value);
-            }
+//            if (!string.IsNullOrEmpty(Value))
+//            {
+//                newNode.AppendAttribute(xmlDoc, TypeName, Value);
+//            }
         }
     }
 }
